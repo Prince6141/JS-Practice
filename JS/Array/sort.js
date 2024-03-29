a897c583ff93ead740a0480771af00134f8bed2c
patel prince <patelprince5121@gmail.com>
2024-03-29 23:47:32 +0530

    Array and sort array

---
+++ JS/Array/sort.js
@@ -0,0 +1,18 @@
+function bubbleSort(arr) {
+  const len = arr.length;
+  for (let i = 0; i < len - 1; i++) {
+    for (let j = 0; j < len - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // Swap elements
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+const arr = [6, 1, 2, 8, 3, 4, 7, 10, 5];
+const sortedArr = bubbleSort(arr);
+console.log(sortedArr);
